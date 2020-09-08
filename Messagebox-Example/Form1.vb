3fa6349d8c965d3431dd872bc2d6844ca758a909
Timothy Axford <35697045+TAxford@users.noreply.github.com>
2020-09-08 06:53:48 -0400

    Adeded Code to closed event
    A message box appears asking if you are sure you want to close.

---
+++ Messagebox-Example/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+        Dim result As DialogResult
+
+        result = MessageBox.Show(“Close form?”, ”quit?”, MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If result = Windows.Forms.DialogResult.No Then
+            e.Cancel = True
+        End If
+
+    End Sub
+End Class
